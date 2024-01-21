9d14dc503c1f2f0e6e20eb2576b77b36738090ba
Pranay Nimsatkar <pranaynimsatkar16@gmail.com>
2024-01-22 00:18:42 +0530

    Create MaximumNumberInAnArray.cpp

---
+++ Arrays/MaximumNumberInAnArray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void findMax(int arr[], int n){
+  int max = INT8_MIN;
+
+  for(int i =0;i<n;i++){
+    if(arr[i]> max){
+      max = arr[i];
+    }
+  }
+  
+  cout<<"Maximum number in the array is "<<max<<endl;
+}
+
+int main() {
+  int arr[5] = {1,2,3,4,10};
+  findMax(arr, 5);
+}

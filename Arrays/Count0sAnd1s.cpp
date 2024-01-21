0636995bd3bc6e1048bbfc8b65daf05d0a669e73
Pranay Nimsatkar <pranaynimsatkar16@gmail.com>
2024-01-22 00:10:52 +0530

    Create Count0sAnd1s.cpp

---
+++ Arrays/Count0sAnd1s.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void counting(int arr[] , int size){
+  int numZero = 0;
+  int numOne = 0;
+
+  for(int i = 0; i < size; i++){
+    if(arr[i] == 0){
+      numZero++;
+    }
+    else{
+      numOne++;
+    }
+  }
+
+  cout<<"Number of zeroes are: "<<numZero<<endl;
+  cout<<"Number of ones are: "<<numOne<<endl;
+  
+}
+
+int main() {
+  int arr[5] = {0,1,0,1,1};
+
+  counting(arr, 5);
+}
